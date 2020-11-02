960329e9b8321535da131d8c1a33ae557ed95da7
[slr1983] <[sumit.l.ramteke@gmail.com]>
2020-11-02 14:23:21 +0530

    Added code to display all Vets closes #2

--- pet-clinic-web/src/main/java/com/sumit/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/sumit/petclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package com.sumit.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sumit.petclinic.services.VetService;
 
 @Controller
 public class VetController {
 	
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+	
 	@RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-	public String showAllVets() {
+	public String showAllVets(Model model) {
+		
+		model.addAttribute("vets", vetService.findAll());
 		
 		return "vets/index";
 		
