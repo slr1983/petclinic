12b8062e78e4498b242b5f76c0f88da42779db4d
[slr1983] <[sumit.l.ramteke@gmail.com]>
2020-11-02 14:09:15 +0530

    Updated to Display Owners List on Owners Page. Closes #1

--- pet-clinic-web/src/main/java/com/sumit/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/sumit/petclinic/controllers/OwnerController.java
@@ -1,13 +1,27 @@
 package com.sumit.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sumit.petclinic.services.OwnerService;
 
 @Controller
 public class OwnerController {
 	
+	private final OwnerService  ownerService;
+	
+	public OwnerController(OwnerService  ownerService) {
+		this.ownerService = ownerService;
+		
+	}
+	
 	@RequestMapping({"/owners", "/owners/index.html", "/owners/index"})
-	public String showAllOwners() {
+	public String showAllOwners(Model model) {
+		
+		
+		model.addAttribute("owners", ownerService.findAll());
+		
 		return "owners/index";
 	}
 
