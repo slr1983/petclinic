1debdeeed8d516c36d881d5ee533a31ec6ad3fe5
[slr1983] <[sumit.l.ramteke@gmail.com]>
2020-11-16 14:49:49 +0530

    Added data for PetType @ Startup. Closes #17

--- pet-clinic-web/src/main/java/com/sumit/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/sumit/petclinic/bootstrap/DataLoader.java
@@ -4,8 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.sumit.petclinic.model.Owner;
+import com.sumit.petclinic.model.PetType;
 import com.sumit.petclinic.model.Vet;
 import com.sumit.petclinic.services.OwnerService;
+import com.sumit.petclinic.services.PetTypeService;
 import com.sumit.petclinic.services.VetService;
 
 @Component
@@ -13,14 +15,24 @@ public class DataLoader implements CommandLineRunner {
 
 	private final OwnerService ownerService;
 	private final VetService vetService;
+	private final PetTypeService petTypeService;
 
-	public DataLoader(OwnerService ownerService, VetService vetService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 		this.ownerService = ownerService;
 		this.vetService =  vetService;
+		this.petTypeService = petTypeService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		PetType saveDogPetType = petTypeService.save(dog);
+		
+		PetType cat = new PetType();
+		cat.setName("Cat");
+		PetType saveCatPetType = petTypeService.save(cat);
 
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Owner1FirstName");
