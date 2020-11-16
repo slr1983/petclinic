7297166db8aa7d29e500ab8ba9520574a877e4f9
[slr1983] <[sumit.l.ramteke@gmail.com]>
2020-11-16 11:42:39 +0530

    Adding Visit Entity and updating Owner Entity. Closes #11

--- pet-clinic-data/src/main/java/com/sumit/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/sumit/petclinic/model/Owner.java
@@ -1,5 +1,7 @@
 package com.sumit.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
 	/**
@@ -7,6 +9,14 @@ public class Owner extends Person {
 	 */
 	private static final long serialVersionUID = 8844833556503648393L;
 
-	
+	private Set<Pet> pets;
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
 
 }
---
+++ pet-clinic-data/src/main/java/com/sumit/petclinic/model/Visit.java
@@ -0,0 +1,48 @@
+package com.sumit.petclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ * Simple JavaBean domain object representing a visit.
+
+ */
+public class Visit extends BaseEntity {
+
+	private LocalDate date;
+
+	private String description;
+
+	private Pet pet;
+
+	/**
+	 * Creates a new instance of Visit for the current date
+	 */
+	public Visit() {
+		this.date = LocalDate.now();
+	}
+
+	public LocalDate getDate() {
+		return this.date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Pet getPet() {
+		return pet;
+	}
+
+	public void setPet(Pet pet) {
+		this.pet = pet;
+	}
+
+}
