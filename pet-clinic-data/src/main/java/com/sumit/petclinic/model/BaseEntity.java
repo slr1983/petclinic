092fe61b7ce3ae6601ae9b7b133bf49d467447b2
[slr1983] <[sumit.l.ramteke@gmail.com]>
2020-11-18 15:07:06 +0530

    Implement Base Entity. Closes #20

--- pet-clinic-data/src/main/java/com/sumit/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/sumit/petclinic/model/BaseEntity.java
@@ -2,6 +2,12 @@ package com.sumit.petclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable{
 	
 	
@@ -9,6 +15,9 @@ public class BaseEntity implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = -4798098062596011401L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
